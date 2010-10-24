9dac304af86d8454dbcc78633595fdb1ca8a4307
Sindre Sørhus <sindresorhus@gmail.com>
2010-10-24 04:49:35 +0200

    Reduced codesize

--- dribbble.php
+++ dribbble.php
@@ -1,81 +1,44 @@
 <?php
-	class Dribbble{
-		public function get_info($username){
-			$curl = curl_init('http://api.dribbble.com/players/'.$username);
+	class Dribbble{		
+		private function get($pathname) {
+			$curl = curl_init("http://api.dribbble.com/" . $pathname);
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
+		}
+		public function get_info($username){
+			return get("players/" . $username);
 		}
 		public function get_shot($id){
-			$curl = curl_init('http://api.dribbble.com/shots/'.$id);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("shots/" . $id);
 		}
 		public function get_rebound($id){
-			$curl = curl_init('http://api.dribbble.com/shots/'.$id.'/rebounds');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("shots/" . $id . "/rebounds");
 		}
 		public function get_comment($id){
-			$curl = curl_init('http://api.dribbble.com/shots/'.$id.'/comments');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("shots/" . $id . "/comments");
 		}
 		public function get_list($list){
-			$curl = curl_init('http://api.dribbble.com/shots/'.$list);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("shots/" . $list );
 		}
 		public function get_shots($username){
-			$curl = curl_init('http://api.dribbble.com/players/'.$username.'/shots/');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("players/" . $username . "/shots/");
 		}
 		public function get_shots_following($username){
-			$curl = curl_init('http://api.dribbble.com/players/'.$username.'/shots/following');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("players/" . $username . "/shots/following");
 		}
 		public function get_id_followers($username){
-			$curl = curl_init('http://api.dribbble.com/players/'.$username.'/followers');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("players/" . $username . "/followers");
 		}
 		public function get_id_following($username){
-			$curl = curl_init('http://api.dribbble.com/players/'.$username.'/following');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("players/" . $username . "/following");
 		}
 		public function get_id_draftees($username){
-			$curl = curl_init('http://api.dribbble.com/players/'.$username.'/draftees');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("players/" . $username . "/draftees");
 		}
 		public function get_id_likes($username){
-			$curl = curl_init('http://api.dribbble.com/players/'.$username.'/shots/likes');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
+			return get("players/" . $username . "/shots/likes");
 		}
 	}
 ?>
