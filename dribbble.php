12820349762ad3185c00b357aac1b803ffef2d13
Sindre Sørhus <sindresorhus@gmail.com>
2010-11-22 20:33:29 +0100

    forgot to add -> to the return statements

--- dribbble.php
+++ dribbble.php
@@ -8,37 +8,37 @@
 			return $response;
 		}
 		public function get_info($username){
-			return get("players/" . $username);
+			return $this->get("players/" . $username);
 		}
 		public function get_shot($id){
-			return get("shots/" . $id);
+			return $this->get("shots/" . $id);
 		}
 		public function get_rebound($id){
-			return get("shots/" . $id . "/rebounds");
+			return $this->get("shots/" . $id . "/rebounds");
 		}
 		public function get_comment($id){
-			return get("shots/" . $id . "/comments");
+			return $this->get("shots/" . $id . "/comments");
 		}
 		public function get_list($list){
-			return get("shots/" . $list );
+			return $this->get("shots/" . $list );
 		}
 		public function get_shots($username){
-			return get("players/" . $username . "/shots/");
+			return $this->get("players/" . $username . "/shots/");
 		}
 		public function get_shots_following($username){
-			return get("players/" . $username . "/shots/following");
+			return $this->get("players/" . $username . "/shots/following");
 		}
 		public function get_id_followers($username){
-			return get("players/" . $username . "/followers");
+			return $this->get("players/" . $username . "/followers");
 		}
 		public function get_id_following($username){
-			return get("players/" . $username . "/following");
+			return $this->get("players/" . $username . "/following");
 		}
 		public function get_id_draftees($username){
-			return get("players/" . $username . "/draftees");
+			return $this->get("players/" . $username . "/draftees");
 		}
 		public function get_id_likes($username){
-			return get("players/" . $username . "/shots/likes");
+			return $this->get("players/" . $username . "/shots/likes");
 		}
 	}
 ?>
